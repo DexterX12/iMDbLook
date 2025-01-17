de158ca9e1cb316e2bb2d18cea646f39b1baa60a
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-01-17 16:26:52 -0500

    movie search has been implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,76 @@ const searchBox = document.querySelector('#movie-search');
 const movieCarouselRight = document.querySelector("#movie-carousel-right");
 const movieCarouselLeft = document.querySelector("#movie-carousel-left");
 const movieCarousel = document.querySelector("#movie-carousel");
-const movieElement = document.querySelector(".movie-element");
+let movieElement = document.querySelector(".movie-element");
+
+const bigScreenVideo = document.querySelector("#background-video");
+const bigScreenTitle = document.querySelector(".movie-big-title");
+const bigScreenDescription = document.querySelector(".movie-big-description");
+
+const imdbAPIbaseURL = "https://imdb.iamidiotareyoutoo.com/";
+
+async function getMedia(query) {
+    const response = await fetch(`${imdbAPIbaseURL}search?q=${query}`);
+    return (await response.json()).description;
+}
+
+async function getMediaDetails(movieID) {
+    const response = await fetch(`${imdbAPIbaseURL}/search?tt=${movieID}`);
+    return await response.json();
+}
+
+async function hasMediaTrailer(movieID) {
+    try {
+        const response = await fetch(`${imdbAPIbaseURL}media/${movieID}`);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+function setMainMedia(mediaObj) {
+    const videoSource = bigScreenVideo.querySelector("source");
+
+    bigScreenTitle.textContent = mediaObj["short"].name;
+    bigScreenDescription.textContent = mediaObj["short"].description;
+
+    if (hasMediaTrailer(mediaObj["imdbId"])) {
+        videoSource.src = `${imdbAPIbaseURL}media/${mediaObj["imdbId"]}`;
+        bigScreenVideo.load();
+    }
+    else {
+        bigScreenVideo.backgroundImage = `url(${mediaObj["short"].image})`;
+    }
+}
+
+function populateMedia(movieList) {
+    movieCarousel.innerHTML = "";
+    for (let movie of movieList) {
+
+        if (!movie["#IMG_POSTER"])
+            continue;
+
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movie-element");
+        movieContainer.style.backgroundImage = `url(${movie["#IMG_POSTER"]})`;
+
+        movieContainer.onclick = async function () {
+            const detailedMedia = await getMediaDetails(movie["#IMDB_ID"]);
+            setMainMedia(detailedMedia);    
+        }
+
+        movieContainer.innerHTML = `
+        <div class="movie-element-title">
+            <p class="movie-title-text">${movie["#TITLE"]} (${movie["#YEAR"]})</p>
+        </div>
+        `;
+
+        movieCarousel.appendChild(movieContainer);
+    }
+}
 
 searchMag.addEventListener('click', function () {
-    searchMag.style.transform = "rotate(250deg)"
+    searchMag.style.transform = "rotate(250deg)";
 
     searchBox.style.animation = "none";
     setTimeout(function () {
@@ -20,17 +86,13 @@ searchMag.addEventListener('click', function () {
     }, 10);
 })
 
-searchBox.addEventListener('focusout', function() {
+searchBox.addEventListener('focusout', async function() {
 
     searchBox.style.animation = "none";
 
     setTimeout(function () {
         searchBox.style.animation = "search-box-anim .5s reverse";
     }, 10);
-
-    if (searchBox.value !== "") {
-        movieCarousel.scrollIntoView({behavior:"smooth"});
-    }
 
     const bringVisibility = function () {
         searchBox.style.visibility = "hidden";
@@ -44,9 +106,16 @@ searchBox.addEventListener('focusout', function() {
     }
 
     searchBox.addEventListener('animationend', bringVisibility);
+
+    if (searchBox.value !== "") {
+        movieCarousel.scrollIntoView({behavior:"smooth"});
+        populateMedia(await getMedia(searchBox.value));
+    }
 });
 
 movieCarouselRight.addEventListener('click', function () {
+    movieElement = document.querySelector(".movie-element");
+
     const scrollLimit = movieCarousel.scrollWidth - movieCarousel.clientWidth;
     const scrollMovement = movieElement.scrollWidth * 2;
 
@@ -63,9 +132,11 @@ movieCarouselRight.addEventListener('click', function () {
             behavior: "smooth"
         });
     }
-})
+});
 
 movieCarouselLeft.addEventListener('click', function () {
+    movieElement = document.querySelector(".movie-element");
+
     const scrollLimit = 0
     const scrollMovement = movieElement.scrollWidth * 2;
 
@@ -82,4 +153,17 @@ movieCarouselLeft.addEventListener('click', function () {
             behavior: "smooth"
         });
     }
-})
+});
+
+
+window.onload = function() {
+
+    getMedia("a")
+    .then(async data => {
+        populateMedia(data);
+
+        const mediaDetails = await getMediaDetails(data[0]["#IMDB_ID"]);
+        setMainMedia(mediaDetails);
+    });
+}
+
